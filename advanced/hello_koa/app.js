87660df1a65ce78152c1d663ceaaf2bae99f6f89
git1oy <luyuhang1991@gmail.com>
2017-11-30 23:31:54 +0800

    Using nunjucks.

--- advanced/hello_koa/app.js
+++ advanced/hello_koa/app.js
@@ -9,6 +9,44 @@ app.use(bodyParser());
 const controller = require('./controllers');
 app.use(controller());
 
+const nunjucks = require('nunjucks');
+function createEnv(path, opts) {
+    var
+        autoescape = opts.autoescape === undefined ? true : opts.autoescape,
+        noCache = opts.noCache || false,
+        watch = opts.watch || false,
+        throwOnUndefined = opts.throwOnUndefined || false,
+        env = new nunjucks.Environment(
+            new nunjucks.FileSystemLoader( 'views', {
+                noCache: noCache,
+                watch: watch,
+            }), {
+                autoescape: autoescape,
+                throwOnUndefined: throwOnUndefined
+            });
+    if (opts.filters) {
+        for (var f in opts.filters){
+            env.addFilter(f, opts.filters[f]);
+        }
+    }
+    return env;
+}
+
+var env = createEnv('views', {
+    watch: true,
+    filters: {
+        hex: function (n) {
+            return '0x' + n.toString(16);
+        }
+    }
+});
+
+// var s = env.render('hello.html', 
+// {
+//      name: "Page", 
+//      fruits: ['Apple', 'Strawberry', 'Watermelon', 'Mongo'] }
+// );
+// console.log(s);
 // app.use(async (ctx, next) => {
 //     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
 //     await next();
--- advanced/hello_koa/controllers/hello.js
+++ advanced/hello_koa/controllers/hello.js
@@ -1,6 +1,45 @@
+const nunjucks = require('nunjucks');
+function createEnv(path, opts) {
+    var
+        autoescape = opts.autoescape === undefined ? true : opts.autoescape,
+        noCache = opts.noCache || false,
+        watch = opts.watch || false,
+        throwOnUndefined = opts.throwOnUndefined || false,
+        env = new nunjucks.Environment(
+            new nunjucks.FileSystemLoader('views', {
+                noCache: noCache,
+                watch: watch,
+            }), {
+                autoescape: autoescape,
+                throwOnUndefined: throwOnUndefined
+            });
+    if (opts.filters) {
+        for (var f in opts.filters) {
+            env.addFilter(f, opts.filters[f]);
+        }
+    }
+    return env;
+}
+
+var env = createEnv('../views', {
+    watch: true,
+    filters: {
+        hex: function (n) {
+            return '0x' + n.toString(16);
+        }
+    }
+});
+
 var fn_hello = async (ctx, next) => {
     var name = ctx.params.name;
-    ctx.response.body = `<h1>Hello, ${name}.</h1>`;
+    var s = env.render('hello.html',
+        {
+            name: name,
+            fruits: ['Apple', 'Strawberry', 'Watermelon', 'Mongo']
+        }
+    );
+    // ctx.response.body = `<h1>Hello, ${name}.</h1>`;
+    ctx.response.body = s;
 };
 
 module.exports = {
