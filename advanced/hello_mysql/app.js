04a44cf5b9dd91266e97575e245a36402f87cb44
git1oy <luyuhang1991@gmail.com>
2017-12-05 23:39:24 +0800

    Operate MySQL with node.js

---
+++ advanced/hello_mysql/app.js
@@ -0,0 +1,58 @@
+const Sequelize = require('sequelize');
+const config = require('./config');
+
+var sequelize = new Sequelize(config.database,
+    config.username, config.password, {
+        host: config.host,
+        dialect: 'mysql',
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 30000
+        }
+    }
+);
+
+var Pet = sequelize.define('pet', {
+    id: {
+        type: Sequelize.STRING(50),
+        primaryKey: true
+    },
+    name: Sequelize.STRING(100),
+    gender: Sequelize.BOOLEAN,
+    birth: Sequelize.STRING(10),
+    createdAt: Sequelize.BIGINT,
+    updatedAt: Sequelize.BIGINT,
+    version: Sequelize.BIGINT
+}, {
+        timestamps: false
+    }
+);
+
+var now = Date.now();
+// Add
+// (async () => {
+//     var cat = await Pet.create({
+//         id: 'm-' + now,
+//         name: 'Oreo',
+//         gender: false,
+//         birth: '2017-05-01',
+//         createdAt: now,
+//         updatedAt: now,
+//         version: 0
+//     });
+//     console.log('created: ' + JSON.stringify(cat));
+// })();
+
+// Query
+(async () => {
+    var pets = await Pet.findAll({
+        where: {
+            name: 'Oreo'
+        }
+    });
+    console.log(`find ${pets.length} pets:`);
+    for (const p of pets) {
+        console.log(JSON.stringify(p));
+    }
+})();
---
+++ advanced/hello_mysql/config.js
@@ -0,0 +1,9 @@
+var config = {
+    database: 'test',
+    username: 'i1oy',
+    password: 'i1oy',
+    host: 'localhost',
+    port: 3306  // MySQL defaut port
+};
+
+module.exports = config;
