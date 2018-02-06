ad6418d7e2b1765c6db99423f789a54a59ceb410
git1oy <luyuhang1991@gmail.com>
2018-02-07 01:12:49 +0800

    Code optimization.

--- advanced/ImageGallery/scripts/showPic.js
+++ advanced/ImageGallery/scripts/showPic.js
@@ -1,8 +1,55 @@
-function showPic(whichPic) {
-    var source = whichPic.getAttribute("href");
-    var text = whichPic.getAttribute("title");
-    var placeholder = document.getElementById("placeholder");
-    var description = document.getElementById("description");
-    placeholder.setAttribute("src", source);
-    description.firstChild.nodeValue = text;
+function prepareGallery() {
+    if (!document.getElementsByTagName) {
+        return false;
+    }
+    if (!document.getElementById) {
+        return false;
+    }
+    if (!document.getElementById("imagegallery")) {
+        return false;
+    }
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for (let index = 0; index < links.length; index++) {
+        const element = links[index];
+        element.onclick = function () {
+            return showPic(this) ? false : true;
+            // return false;
+        }
+    }
 }
+
+function showPic(whichPic) {
+    var placeholder = document.getElementById("placeholder");
+    if (!placeholder) {
+        return false;
+    }
+    // var source = whichPic.getAttribute("href");
+    var source = whichPic.href;
+    // placeholder.setAttribute("src", source);
+    placeholder.src = source;
+
+    var description = document.getElementById("description");
+    if (description) {
+        var text = whichPic.getAttribute("title") ? whichPic.getAttribute("title") : "";
+        description.firstChild.nodeValue = text;        
+    }
+
+    return true;
+}
+
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+
+addLoadEvent(prepareGallery);
+
+// window.onload = prepareGallery;
